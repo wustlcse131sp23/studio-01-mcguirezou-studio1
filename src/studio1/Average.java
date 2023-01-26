28b35ab84966d29837cf7b06b9fb2a25fe048afc
jacka <jacka@LAPTOP-5LVAT6CL.dhcp.wustl.edu>
2023-01-26 13:52:11 -0600

    :)

---
+++ src/studio1/Average.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a first integer: ");
+		int n1 = in.nextInt();
+		System.out.println("Enter a second integer: ");
+		int n2 = in.nextInt();
+		System.out.println("Average of " + n1 + " and " + n2 + " is " + ((n1 + n2)/2.0) + ".");
+		
+		
+
+	}
+
+}
